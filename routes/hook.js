628b8eb9d43e2b9fb34ba1eaa43c41f0755dff2a
Joel Albert <jalbert@highschoolcube.com>
2019-03-09 11:43:47 -0600

    listen on get

--- routes/hook.js
+++ routes/hook.js
@@ -3,12 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
+  var io = req.app.get("socketio");
   let bodys = req.body;
   console.log(bodys);
+  io.emit("sms", "This is amazing");
   res.send("Nothing here for you");
 });
 
-router.post("/", function(req, res) {
+router.post("/", function(req, res, next) {
   var io = req.app.get("socketio");
   let bodys = req.query;
   console.log(bodys);
