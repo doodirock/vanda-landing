7b682d9135abb41582bcc1db384178dc4b24cbb8
Joel Albert <jalbert@highschoolcube.com>
2019-03-08 18:43:48 -0600

    set up gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-	watch: true,
-	mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-	entry: ['./src/index.scss'],
-	output: {
-		path: path.resolve(__dirname, 'assets')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.scss$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: 'css/index.css',
-						}
-					},
-					{
-						loader: 'extract-loader'
-					},
-					{
-						loader: 'css-loader'
-					},
-					{
-						loader: 'sass-loader'
-					}
-				]
-			}
-		]
-	}
+  watch: true,
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: ["./src/index.scss"],
+  output: {
+    path: path.resolve(__dirname, "assets")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "css/index.css"
+            }
+          },
+          {
+            loader: "extract-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  }
 };
